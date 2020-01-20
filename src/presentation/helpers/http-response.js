8e96eeb0dd9875277a02f2b12295e3fa6d9effe3
Fellipe Botelho <fellipegpbotelho@gmail.com>
2020-01-20 00:09:00 -0300

    Move server error to a helper method

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,4 +1,5 @@
 const MissingParamError = require('./missing-param-error')
+const ServerError = require('./server-error')
 const UnauthorizedError = require('./unauthorized-error')
 
 module.exports = class HttpResponse {
@@ -7,7 +8,7 @@ module.exports = class HttpResponse {
   }
 
   static serverError () {
-    return { statusCode: 500 }
+    return { statusCode: 500, body: new ServerError() }
   }
 
   static unauthorizedError () {
